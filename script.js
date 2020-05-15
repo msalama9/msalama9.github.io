0de8a09b45463cfd7d35c87a48bd59fbc949be4e
msalama9 <57646267+msalama9@users.noreply.github.com>
2020-05-15 00:53:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 const navSlide = () => {
   const burger = document.querySelector("button");
+  const parentNode = document.querySelector(".item-one").parentNode;
+  const firstItem = document.querySelector(".item-one");
   const nav = document.querySelector(".burger-nav");
+  const list = document.querySelector("ul");
   const closeButton = document.querySelector(".close");
+  const listItem = document.createElement("li");
+  const link = document.createElement("a");
 
   burger.addEventListener("click", () => {
     nav.classList.add("nav-in");
+    link.classList.add("closeButton");
+    listItem.appendChild(link);
+    link.innerHTML = "X";
+    parentNode.insertBefore(listItem, firstItem);
   });
 
-  closeButton.addEventListener("click", () => {
+  link.addEventListener("click", () => {
     nav.classList.remove("nav-in");
+    parentNode.removeChild(listItem);
   });
 };
 
